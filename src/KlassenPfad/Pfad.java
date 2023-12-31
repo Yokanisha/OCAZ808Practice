2e2969c418a29bc86e619529fdc95e0e875d7c1b
Fatih <fatihzkn@web.de>
2023-12-31 20:44:20 +0100

    commit 16

--- src/KlassenPfad/Pfad.java
+++ src/KlassenPfad/Pfad.java
@@ -1,20 +1,57 @@
 package KlassenPfad;
 
 public class Pfad {
+    int pfad;
+    Pfad link;
+    Pfad(int pfad) {
+         this.pfad = pfad;
+         {
+            System.out.println(" block konstruktor");
+        }
+    }
 
-    static int[] x = new int[1];
+    int myValue = 0;
 
+    static int staticc = staticMethode();
 
-
+    static {
+        System.out.println("static block");
+    }
+char aaa = 1222;
     public static void main(String[] args) {
 
-        x[0] = 10;
+        Pfad a = new Pfad(1);
+        Pfad b = new Pfad(2);
+
+        a.link = b;
+
+        a.showOne(21);
+    }
+
+    static int staticMethode() {
+        System.out.println("static methode");
+        return 1;
+    }
+
+    public void showOne(int myValue){
+        myValue = myValue;
+    }
+
+    public void showTwo(int myValue){
+        this.myValue = myValue;
+    }
 
 
 
-        }
+
+
 
 }
 
+
 class A { }
 class B extends A { }
+
+
+
+
